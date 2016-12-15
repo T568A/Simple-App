5474095acd59bf3eebd7ac631d120dd60dbec231
T568A <andrey.lopatkin@outlook.com>
2016-12-15 22:25:00 +0500

    favicon

--- app/Models/DbConnect.php
+++ app/Models/DbConnect.php
@@ -4,16 +4,17 @@ namespace App\Models;
 
 final class DbConnect
 {
-    public static $dsn;
-    public static $user;
-    public static $pass;
-    public static $opts;
+    public static $dsn = 'mysql:host=localhost;dbname=DB;charset=utf8';
+    public static $user = 'root';
+    public static $pass = '';
+    public static $opts = '';
     private static $dbh;
 
-    public static function get() {
+    public static function get()
+    {
         try {
             if (null === self::$dbh) {
-                    self::$dbh = new \PDO(self::$dsn, self::$user, self::$pass);
+                self::$dbh = new \PDO(self::$dsn, self::$user, self::$pass);
             }
         } catch (\PDOException $e) {
             throw new \Exception('Error establishing a database connection!');
@@ -21,7 +22,7 @@ final class DbConnect
         return self::$dbh;
     }
 
-    private function __construct() { }
+    private function __construct() {}
     private function __clone() {}
     private function __wakeup() {}
 }
