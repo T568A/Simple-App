a3ecbabb805d1366722c2c465ca7f75edff597ab
T568A <andrey.lopatkin@outlook.com>
2016-12-27 19:15:31 +0500

    Travis CI

--- app/Models/DbConnect.php
+++ app/Models/DbConnect.php
@@ -7,17 +7,17 @@ final class DbConnect
     public static $dsn = 'mysql:host=localhost;dbname=DB;charset=utf8';
     public static $user = 'root';
     public static $pass = '';
-    public static $opts = '';
+    public static $opts = [];
     private static $dbh;
 
     public static function get()
     {
         try {
             if (null === self::$dbh) {
-                self::$dbh = new \PDO(self::$dsn, self::$user, self::$pass);
+                self::$dbh = new \PDO(self::$dsn, self::$user, self::$pass, self::$opts);
             }
         } catch (\PDOException $e) {
-            throw new \Exception('Error establishing a database connection!');
+            throw new \Exception('Error establishing a database connection!' . PHP_EOL . $e->getMessage());
         }
         return self::$dbh;
     }
